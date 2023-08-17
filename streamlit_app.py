f435252b0625465c92ee7551ad2c261e7540614f
Angel Rys <reyes>
2023-08-17 01:21:03 -0500

    fixed venmo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+#st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://https://github.com/AngelR0/MariachiADALinks)")
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('pic.jpg'))
@@ -12,12 +12,12 @@ col2.image(Image.open('pic.jpg'))
 st.header('Mariachi Aguilas De America')
 
 st.info('Adept for any type of events.')
+st.info('Zelle: (469)734-7799   Ines Adrian Reyes')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
+st_button('youtube', 'https://www.youtube.com/@mariachiaguilasdeamericade2728/videos', 'Mariachi Aguilas De America Youtube channel', icon_size)
+st_button('venmo', 'https://venmo.com/u/MariachiADA', 'Tip or Pay via Venmo', icon_size)
 st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
